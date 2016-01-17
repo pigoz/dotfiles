3851c52853d284e26ac3a887704bb5981868abf0
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2016-01-17 10:40:01 +0100

    mpv: add many interpolation scripts

--- mpv/filters/interpolation-block.py
+++ mpv/filters/interpolation-block.py
@@ -4,20 +4,19 @@ core = vs.get_core()
 core.std.LoadPlugin(path="/usr/local/lib/libmvtools.dylib")
 
 # skip motion interpolation completely for content exceeding the limits below
-max_fps = 60
-
+max_width  = 1920
+max_height = 1200
+max_fps    = 60
 # use BlockFPS instead of FlowFPS for content exceeding the limits below
 # a block is considered as changed when 8*8*x > th_block|flow_diff with 8*8
 # being the size of a block (scaled internally) and x the difference of each
 # pixel within this block, default 400
-th_flow_diff  = 8*8*7
-
+th_block_diff  = 8*8*7
 # (threshold/255)% blocks have to change to consider this a scene change
 # (= no motion compensation), default 130
-th_flow_changed  = 14
-
+th_block_changed  = 14
 # size of blocks the analyse step is performed on
-blocksize = 32
+blocksize = 16
 
 # assume display_fps to be bogus when not in a certain range
 target_num = 60 if display_fps < 23.9 or display_fps > 300 else display_fps
@@ -30,30 +29,19 @@ source_den = int(1e6)
 
 clip = video_in
 
-def interpolate(clip):
+if not (clip.width > max_width or clip.height > max_height or container_fps > max_fps):
     clip = core.std.AssumeFPS(clip, fpsnum=source_num, fpsden=source_den)
-    super_args = {
-        'pel': 2, # half-pel, 4 is qpel too slow for realtime
-        'sharp': 0, # downscaling filter
-        'hpad': blocksize,
-        'vpad': blocksize
-    }
-
-    sup  = core.mv.Super(clip, **super_args)
-
+    sup  = core.mv.Super(clip, pel=2, hpad=blocksize, vpad=blocksize, sharp=1)
     bv   = core.mv.Analyse(sup, blksize=blocksize, isb=True , chroma=True, search=3, searchparam=2)
     fv   = core.mv.Analyse(sup, blksize=blocksize, isb=False, chroma=True, search=3, searchparam=2)
-    clip = core.mv.FlowFPS(clip, sup, bv, fv, num=target_num, den=target_den,
-                           mask=0, thscd1=th_flow_diff, thscd2=th_flow_changed,
-                           blend = False)
+    clip = core.mv.BlockFPS(clip, sup, bv, fv, num=target_num, den=target_den,
+                            mode=3, thscd1=th_block_diff, thscd2=th_block_changed)
 
-    return clip
-
-if(container_fps < max_fps):
-    clip = interpolate(clip)
-    print('motion-interpolation: {0} -> {1} FPS'
-          .format(source_num / source_den, target_num / target_den))
+    print('motion-interpolation: {0} -> {1} FPS | blockfps | {2} Hz'
+          .format(source_num / source_den, target_num / target_den,
+                  display_fps))
 else:
-    print('motion-interpolation: skipping {2} FPS video'.format(container_fps))
+    print('motion-interpolation: skipping {0}x{1} {2} FPS video'
+          .format(clip.width, clip.height, container_fps))
 
 clip.set_output()
--- mpv/filters/interpolation-hq.py
+++ mpv/filters/interpolation-hq.py
@@ -5,19 +5,22 @@ core.std.LoadPlugin(path="/usr/local/lib/libmvtools.dylib")
 
 # skip motion interpolation completely for content exceeding the limits below
 max_fps = 60
-
 # use BlockFPS instead of FlowFPS for content exceeding the limits below
+max_flow_width  = 1280
+max_flow_height = 720
 # a block is considered as changed when 8*8*x > th_block|flow_diff with 8*8
 # being the size of a block (scaled internally) and x the difference of each
 # pixel within this block, default 400
+th_block_diff = 8*8*7
 th_flow_diff  = 8*8*7
-
 # (threshold/255)% blocks have to change to consider this a scene change
 # (= no motion compensation), default 130
+th_block_changed = 14
 th_flow_changed  = 14
-
 # size of blocks the analyse step is performed on
-blocksize = 32
+blocksize = 16
+
+force_block = False
 
 # assume display_fps to be bogus when not in a certain range
 target_num = 60 if display_fps < 23.9 or display_fps > 300 else display_fps
@@ -30,30 +33,25 @@ source_den = int(1e6)
 
 clip = video_in
 
-def interpolate(clip):
+if not (container_fps > max_fps):
     clip = core.std.AssumeFPS(clip, fpsnum=source_num, fpsden=source_den)
-    super_args = {
-        'pel': 2, # half-pel, 4 is qpel too slow for realtime
-        'sharp': 0, # downscaling filter
-        'hpad': blocksize,
-        'vpad': blocksize
-    }
-
-    sup  = core.mv.Super(clip, **super_args)
-
+    sup  = core.mv.Super(clip, pel=4, hpad=blocksize, vpad=blocksize, sharp=2)
     bv   = core.mv.Analyse(sup, blksize=blocksize, isb=True , chroma=True, search=3, searchparam=2)
     fv   = core.mv.Analyse(sup, blksize=blocksize, isb=False, chroma=True, search=3, searchparam=2)
-    clip = core.mv.FlowFPS(clip, sup, bv, fv, num=target_num, den=target_den,
-                           mask=0, thscd1=th_flow_diff, thscd2=th_flow_changed,
-                           blend = False)
 
-    return clip
-
-if(container_fps < max_fps):
-    clip = interpolate(clip)
-    print('motion-interpolation: {0} -> {1} FPS'
-          .format(source_num / source_den, target_num / target_den))
+    use_block = clip.width > max_flow_width or clip.height > max_flow_height
+    if force_block or use_block:
+        clip = core.mv.BlockFPS(clip, sup, bv, fv, num=target_num, den=target_den,
+                                mode=3, thscd1=th_block_diff, thscd2=th_block_changed)
+    else:
+        clip = core.mv.FlowFPS(clip, sup, bv, fv, num=target_num, den=target_den,
+                               mask=0, thscd1=th_flow_diff, thscd2=th_flow_changed)
+    print('motion-interpolation: {0} -> {1} FPS | {3} | {2} Hz'
+          .format(source_num / source_den, target_num / target_den,
+                  display_fps, use_block and "block" or "flow"))
 else:
-    print('motion-interpolation: skipping {2} FPS video'.format(container_fps))
+    print('motion-interpolation: skipping {0}x{1} {2} FPS video'
+          .format(clip.width, clip.height, container_fps))
 
 clip.set_output()
+
