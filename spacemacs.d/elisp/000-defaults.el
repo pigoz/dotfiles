33d3614a8b8e0114c836037a6d80cd386d01fe7e
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2017-09-16 09:27:56 +0200

    emacs: add spell/syntax check, and elm layer

--- spacemacs.d/elisp/000-defaults.el
+++ spacemacs.d/elisp/000-defaults.el
@@ -43,11 +43,12 @@ values."
      ruby
      ;; javascript
      react
+     elm
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
+     spell-checking
+     syntax-checking
      ;; version-control
      )
    ;; List of additional packages that will be installed without being
