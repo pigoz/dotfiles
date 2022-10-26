b7a355a7a635b18ce07322f5cda01a978953f85e
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2022-10-26 09:37:30 +0200

    vim: prevent leap from mapping x

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -111,6 +111,8 @@ return require('user.packer').setup(function(use)
     'ggandor/leap.nvim',
     config = function()
       require('leap').add_default_mappings()
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
     end
   }
 
