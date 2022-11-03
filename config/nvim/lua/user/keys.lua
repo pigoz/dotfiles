3e2de4b42ed1cd7ee8aa1d6ca32258670b659868
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2022-11-03 09:53:32 +0100

    vim: make lazygit bigger

--- config/nvim/lua/user/keys.lua
+++ config/nvim/lua/user/keys.lua
@@ -157,8 +157,8 @@ function M.telescope_help()
 end
 
 function M.lazygit()
-  vim.g.floaterm_height = 0.98
-  vim.g.floaterm_width = 0.98
+  vim.g.floaterm_height = 0.999
+  vim.g.floaterm_width = 0.999
   vim.g.floaterm_title = ''
   vim.g.floaterm_opener = 'tabe'
   vim.cmd(':FloatermNew --name=lazygit lazygit')
