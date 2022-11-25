631a9f24d55ddf3998d94b212eab470287790410
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2022-11-25 17:39:30 +0100

    vim: actually enable treesitter highlighting

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -104,6 +104,11 @@ return require("user.packer").setup(function(use)
       require("nvim-treesitter.configs").setup({
         -- A list of parser names, or "all"
         ensure_installed = { "c", "lua", "typescript", "ruby", "scss", "css" },
+
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
       })
     end,
   })
